6fc3c29d80290a7a55fc4d85cd390409cd482888
Nate <nathan.wangemann@aggiemail.usu.edu>
2020-01-08 15:38:18 -0700

    complete exercises with code

--- practice.js
+++ practice.js
@@ -14,60 +14,75 @@
   Then alert your name using dot notation.
 */
 
-//Code here
+var me = {
+  name: 'Nate',
+  age: 100
+}
+alert(me.name);
 
 ////////// PROBLEM 2 //////////
 
-/*
-  Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday.
-  Have the values to those keys be strings that are equal to your favorite thing in that category.
-*/
+// /*
+//   Make a 'favoriteThings' object that contains the following keys: band, food, person, book, movie, holiday.
+//   Have the values to those keys be strings that are equal to your favorite thing in that category.
+// */
 
-//Code here
+let favoriteThings = {
+  band: "Grateful Dead",
+  food: "Cheesecake",
+  person: "My mom",
+  book: "Lord of the Rings",
+  movie: "Harry Potter",
+  holiday: "Halloween"
+}
 
-/*
-  After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
-  and then another key named 'brand' with the value being your favorite brand.
-*/
+// /*
+//   After you've made your object, use bracket or dot notation to add another key named 'car' with the value being your favorite car
+//   and then another key named 'brand' with the value being your favorite brand.
+// */
 
-//Code here
+favoriteThings.car = "Subaru"
+favoriteThings.brand = "None"
 
-/*
-  Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
-  and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
-*/
+// /*
+//   Now use bracket or dot notation to change the value of the food key in your favoriteThings object to be 'Chicken Nuggets'
+//   and change the value of the book key in your favoriteThings object to be 'Harry Potter'.
+// */
 
-//Code here
+favoriteThings.food = "Chicken Nuggets"
+favoriteThings.book = "Harry Potter"
 
-////////// PROBLEM 3 //////////
+// ////////// PROBLEM 3 //////////
 
-/*
-  Create an empty Object called backPack.
-  Now, create a variable called item and set it equal to the string 'firstPocket'.
-  Using bracket notation and the item variable, add a 'firstPocket' key (or property) to backPack.
-  Set the value of that key to 'chapstick'.
-  Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
-*/
+// /*
+//   Create an empty Object called backPack.
+//   Now, create a variable called item and set it equal to the string 'firstPocket'.
+//   Using bracket notation and the item variable, add a 'firstPocket' key (or property) to backPack.
+//   Set the value of that key to 'chapstick'.
+//   Using dot notation, add another key (or property) to your backPack object that is named color, with the value being the color of your backpack.
+// */
 
-//Code here
+var backPack = {
 
-/*
-  After you do the above, alert your entire backPack object.
-*/
+}
+var item = 'firstPocket'
+backPack['firstPocket'] = 'chapstick';
+backPack.color = 'green'
+// // /*
+//   After you do the above, alert your entire backPack object.
+// */
 
-//Code here
+// alert(backPack);
+// /*
+// You probably noticed that it just alerted [object Object].
+// Alerting to see the data in your Object doesn't work so well.
+// Instead, console.log your whole backPack object and then check out the console.
+// // */
 
-/*
-You probably noticed that it just alerted [object Object].
-Alerting to see the data in your Object doesn't work so well.
-Instead, console.log your whole backPack object and then check out the console.
-*/
+// // console.log(backPack);
+// // ////////// PROBLEM 4 //////////
 
-//Code here
-
-////////// PROBLEM 4 //////////
-
-// Do not edit the code below.
+// // // Do not edit the code below.
 var user2 = {
   name: 'Bryan',
   age: 24,
@@ -76,54 +91,74 @@ var user2 = {
   birthday: '05/02/1990',
   username: 'bryansmith33'
 };
-// Do not edit the code above.
+// // // Do not edit the code above.
 
-/*
-  Let's say I, the user, decided to change my name and email address to the following:
-  name -> 'Bryan G. Smith' and email -> 'bryan.smith@devmounta.in'.
-  Make that change without modifying the original object code above.
-*/
+// // /*
+// //   Let's say I, the user, decided to change my name and email address to the following:
+// //   name -> 'Bryan G. Smith' and email -> 'bryan.smith@devmounta.in'.
+// //   Make that change without modifying the original object code above.
+// // */
 
-//Code Here
+user2.name = 'Bryan G. Smith'
+user2.email = 'bryan.smith@devmounta.in'
 
-/////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
-////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
+// // /////////////////////// EXTRA PRACTICE PROBLEMS BELOW ////////////////////
+// // ////////// MOVE ONTO NEXT SECTION BEFORE WORKING ON THESE ////////////////
 
-////////// PROBLEM 5 //////////
+// // ////////// PROBLEM 5 //////////
 
-/*
-  Create an empty object called methodCollection.
-*/
+// // /*
+// //   Create an empty object called methodCollection.
+// // */
 
-//Code Here
+var methodCollection = {
 
-/*
-  Now add two methods (functions that are properties on objects) to your methodCollection object.
-  One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
-*/
+}
+// // /*
+// //   Now add two methods (functions that are properties on objects) to your methodCollection object.
+// //   One called 'alertHello' which alerts 'hello' and another method called 'logHello' which logs 'hello' to the console.
+// // */
 
-//Code Here
+methodCollection.alertHello = function alertHello(){
+  alert('hello');
+}
+methodCollection.logHello = function () {
+  console.log('hello');
+}
 
-/*
-  Now call your alertHello and logHello methods.
-*/
+// // /*
+// //   Now call your alertHello and logHello methods.
+// // */
 
-//Code Here
+methodCollection.alertHello();
+methodCollection.logHello();
 
-////////// PROBLEM 6 //////////
+// // ////////// PROBLEM 6 //////////
 
-/*
-  Create a function called makePerson which takes in name, birthday, ssn as its parameters.
-  Return a new object with all of the information that you passed in.
-*/
+// // /*
+// //   Create a function called makePerson which takes in name, birthday, ssn as its parameters.
+// //   Return a new object with all of the information that you passed in.
+// // */
 
-//Code Here
+function makePerson (name, birthday, ssn){
+  return {
+    name: name,
+    birthday: birthday,
+    ssn: ssn
+  }
+}
 
-////////// PROBLEM 7 //////////
+// // ////////// PROBLEM 7 //////////
 
-/*
-  Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
-  Return that object so that whenever you invoke makeCard, you get a brand new credit card.
-*/
+// // /*
+// //   Create a function called makeCard which takes in cardNumber, expirationDate, and securityCode to make a Credit Card object.
+// //   Return that object so that whenever you invoke makeCard, you get a brand new credit card.
+// // */
 
-//Code Here
+function makeCard (cardNumber, expirationDate, securityCode){
+  return {
+    cardNumber: cardNumber,
+    expirationDate: expirationDate,
+    securityCode: securityCode
+  }
+}
